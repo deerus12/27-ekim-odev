8c2c2b47f18aafb9a68d459b8475c0212306c7cd
90505 <90505@DESKTOP-SR3BTCD>
2024-10-26 16:49:48 +0300

    Add project files.

---
+++ ODEV/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ODEV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                int sonuc = 0;
+                int sayi = Convert.ToInt32(textBox1.Text);
+                for (int i = 1; i <= sayi; i++)
+                {
+                    sonuc += i;
+                }
+                MessageBox.Show("toplam"+ sonuc);
+            }
+        }
+}
---
+++ ODEV/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ODEV
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 10; i <= 110; i = i + 10)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
